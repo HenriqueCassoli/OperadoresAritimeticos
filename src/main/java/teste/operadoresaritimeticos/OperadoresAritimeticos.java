a21df40e0c671766393e7d4552e398599cde89a5
Cassoli <fernandes.cassoli@gmail.com>
2024-02-03 17:58:29 -0300

    first commit

---
+++ src/main/java/teste/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package teste.operadoresaritimeticos;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class OperadoresAritimeticos {
+
+    public static void main(String[] args) {
+        /*
+        int n1 = 3;
+        int n2 = 5;
+        float m = n1+n2/2;
+        System.out.println("A média é igual a " + m); 
+        */
+       /* int numero = 10;
+        int valor = 4 + --numero;
+        System.out.println(valor);
+        System.out.println(numero);*/
+       
+      /*  int x =4;
+       x *=2; // x = x + 2
+      System.out.println(x);*/
+      
+     /*  float v = 8.1f;
+      int ar = (int) Math.ceil(v);
+        System.out.println(ar);*/
+     double ale =Math.random();
+        System.out.println(ale);
+    }
+}
